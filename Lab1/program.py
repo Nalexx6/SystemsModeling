cbddd65ec3690cb7ab9fac750148d35ae3c4e5a1
Nalexx6 <nikita2002oleksienko@gmail.com>
2021-10-04 13:51:03 +0300

    Fourier transformation fixed

--- Lab1/program.py
+++ Lab1/program.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.signal import argrelextrema
 
 # Reading data from file
 data_str = open("./f6.txt").read().split()
@@ -12,30 +13,15 @@ time = np.arange(0, T + dt, dt)
 
 # Finding Fourier transforms
 n = time.shape[0]
-frequency = np.zeros(n)
+N = np.arange(n)
+k = N.reshape((n, 1))
+M = np.exp(-2j * np.pi * k * N / n)
 
-for point_id in range(n):
-    sin_freq = 0
-    cos_freq = 0
-
-    for signal in range(n):
-        sin_freq += data[signal] * np.sin(2. * np.pi * point_id * signal / float(n))
-        cos_freq += data[signal] * np.cos(2. * np.pi * point_id * signal / float(n))
-
-    sin_freq /= float(n)
-    cos_freq /= float(n)
-
-    frequency[point_id] = np.sqrt(sin_freq ** 2 + cos_freq ** 2)
-
-# Finding local maximum
-biggest_value = []
-
-for i in range(3, n // 2):
-    if np.max(frequency[i - 3:i + 3]) == frequency[i]:
-        biggest_value.append(i)
-        print(frequency[i])
-
-main_frequency = biggest_value[0] / T   # <- local maximum
+transformed_data = np.dot(M, data) / n
+transformed_half = transformed_data[:transformed_data.shape[0] // 2 - 1]
+extremums = np.array(argrelextrema(transformed_half, np.greater))
+main_frequency = extremums[0][0]/T
+print(main_frequency)
 
 # Apply least squares method to find resulted approximation
 b = np.array([np.sum(data * time ** 3), np.sum(data * time ** 2), np.sum(data * time),
@@ -55,10 +41,9 @@ print(solution)
 approximated_func = np.dot(solution, functions)     # <- approximated function by its values
 
 # Displaying data from all steps. Also we could see that function approximation was done well
-fig, axs = plt.subplots(3)
+fig, axs = plt.subplots(2)
 plt.grid(True)
 fig.suptitle("Data, Frequencies and Resulted approximation")
 axs[0].plot(time, data)
-axs[1].plot(time, frequency)
-axs[2].plot(time, approximated_func)
+axs[1].plot(time, approximated_func)
 plt.show()
